68effd18e79c3a1cc751b3338aa31f9ef021a765
Elise <edeglau.classic@gmail.com>
2020-07-08 22:24:47 -0700

    Update learn_panels_model.py

--- gitHub/toolbase/learn_panels_model.py
+++ gitHub/toolbase/learn_panels_model.py
@@ -11,7 +11,13 @@
         gotten_path = "{}/PRODUCTS/images/{}/{}/playblast-half1712x903_vdf8_jpg/{}".format(folder_strt, _dept_task, scn_name, gotten_name)
         mc.playbackOptions(fps = fps_set, e=1)
         sel_NumMn=mc.playbackOptions(q=1, min=1)
-        sel_NumMn_reset=mc.playbackOptions(q=1, min=1)+1
+        sel_NumMn_reset=mc.playbackOptions(q=1, min=1)+1    
         mc.currentTime(sel_NumMn)
         mc.currentTime(sel_NumMn_reset)        
-        mc.c
+        mc.currentTime(sel_NumMn)
+        mc.playblast(format="image", filename=gotten_path, sequenceTime=0, clearCache=1, viewer= 1,  showOrnaments=1, offScreen=1, fp=4, percent = 100, compression = "jpg",  quality = 100, widthHeight = (1712, 903))
+        mc.playbackOptions(fps = 1.0, e=1)        
+        mm.eval('setPolyCountVisibility(0);')
+        for each in focPane:
+            if mc.modelEditor(each, q=1, av=1) == True:
+                mc.modelEditor(each, e=1, rnm="base_OpenGL_Renderer") 
