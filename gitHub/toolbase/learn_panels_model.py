0d4bb01b2f1644dac37349c647d8f075ad714aaf
Elise <edeglau.classic@gmail.com>
2020-07-08 22:42:14 -0700

    prvew addons

--- gitHub/toolbase/learn_panels_model.py
+++ gitHub/toolbase/learn_panels_model.py
@@ -1,5 +1,79 @@
 from toolbase.cache_stuff import wk_strt_value
+import os, sys, subprocess
+import maya.cmds as mc
+import maya.mel
+from sys import argv
+import collections
 
+import PyQt4
+from PyQt4 import QtCore, QtGui, Qt
+from PyQt4.QtGui import QWidget, QRadioButton, QGridLayout, QLabel, \
+    QTableWidget, QComboBox, QKeySequence, QToolButton, QPlainTextEdit, QPushButton,QBoxLayout, \
+    QClipboard, QTableWidgetItem, QCheckBox, QVBoxLayout, QHBoxLayout, \
+    QPixmap, QLineEdit, QListWidget, QTextEdit, QSizePolicy, QFrame, QPalette, QColor, \
+    QFont, QAbstractItemView, QMenu, QMessageBox
+from PyQt4.QtCore import SIGNAL
+from Cython.Utility.MemoryView import item
+
+class prvw_UI(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(prvw_UI, self).__init__(parent = None)
+ 
+        self.setWindowTitle("Preview setups")
+        self.central_widget=QtWidgets.QWidget(self)
+        self.setCentralWidget(self.central_widget)
+        self.masterLayout=QtWidgets.QGridLayout(self.central_widget)
+        self.masterLayout.setAlignment(QtCore.Qt.AlignTop)
+ 
+ 
+        self.myform = QtWidgets.QFormLayout()
+        self.layout = QtWidgets.QGridLayout()
+        self.masterLayout.addLayout(self.layout, 0,0,1,1)
+ 
+        self.SelectionSetupLayout = QtWidgets.QGridLayout()
+        self.selection_widgetframe = QtWidgets.QFrame()
+        self.selection_widgetframe.setLayout(self.SelectionSetupLayout)
+        self.SelectionSetupLayout.addLayout(self.myform, 0,0,1,1)
+        self.layout.addLayout(self.SelectionSetupLayout, 0,0,1,1)
+ 
+        self.add_widgets()
+ 
+        scroll = QtWidgets.QScrollArea()
+        scroll.setWidget(self.selection_widgetframe)
+        scroll.setWidgetResizable(False)
+        self.layout.addWidget(scroll, 1,0,1,1)
+        self.setLayout(self.layout)
+
+    def add_widgets(self):
+        self.duplct_order_layout = QtWidgets.QHBoxLayout()
+        self.myform.addRow(self.duplct_order_layout)
+        self.dup_layout = QtWidgets.QVBoxLayout()
+        self.duplct_order_layout.addLayout(self.dup_layout)
+        self.duplct_slid_layout = QtWidgets.QVBoxLayout()
+        self.duplct_order_layout.addLayout(self.duplct_slid_layout)  
+        self.duplct_slider_layout = QtWidgets.QVBoxLayout()    
+        self.duplct_slid_layout.addLayout(self.duplct_slider_layout)  
+        
+        self.lbl_substp = QtWidgets.QLabel("playback substep")
+        self.dup_layout.addWidget(self.duplctlbl_substp_nm_obj_button)
+        self.substp_amnt = QtWidgets.QLineEdit("1.0")
+        self.dup_layout.addWidget(self.substp_amnt)
+        
+        self.duplct_nm_obj_button = QtWidgets.QPushButton("blast")
+        self.connect(self.duplct_nm_obj_button, SIGNAL("clicked()"),
+                    lambda: self.print_dups())
+        self.dup_layout.addWidget(self.duplct_nm_obj_button)
+        
+        self.duplct_sl_obj_button = QtWidgets.QPushButton("Mem blast")
+        self.connect(self.duplct_sl_obj_button, SIGNAL("clicked()"),
+                    lambda: self.select_dups())
+        self.dup_layout.addWidget(self.duplct_sl_obj_button) 
+        
+        self.duplct_rnm_button = QtWidgets.QPushButton("Mem Blast No Roll")
+        self.connect(self.duplct_rnm_button, SIGNAL("clicked()"),
+                    lambda: self.rename_dups())
+        self.dup_layout.addWidget(self.duplct_rnm_button) 
+        
     def lights_cam(self):
         if mc.objExists("*:camlight_loc"):
             print "cam lights already exist - won't import"
@@ -143,3 +217,6 @@ from toolbase.cache_stuff import wk_strt_value
                 mc.setAttr("hardwareRenderingGlobals.ssaoAmount", 0)
                 mc.setAttr("hardwareRenderingGlobals.ssaoRadius", 7)
                 mc.setAttr("hardwareRenderingGlobals.ssaoFilterRadius", 16)
+                
+inst_do_win = prvw_UI()
+inst_do_win.show()
